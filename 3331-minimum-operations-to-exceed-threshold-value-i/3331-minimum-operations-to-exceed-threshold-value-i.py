f71df6d53144558b815e6a453e579ef8ca0f93ed
Sai Surya Vinay <147791886+saisuryavinay@users.noreply.github.com>
2025-06-27 19:55:06 +0530

    Delete 3331-minimum-operations-to-exceed-threshold-value-i directory

--- 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minOperations(self, nums: List[int], k: int) -> int:
-        # sync to GitHub
-        count = 0
-        n = len(nums)
-        for i in range(n):
-            if nums[i]<k:
-                count+=1
-        return count
