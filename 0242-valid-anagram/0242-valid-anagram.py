4e89f44192ccd16cc00946d5e130649fca0db75c
Devu Sai Surya Vinay <147791886+saisuryavinay@users.noreply.github.com>
2026-09-20 10:35:56 +0530

    Time: 11 ms (77.57%), Space: 19.3 MB (76.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,7 +4,7 @@ class Solution:
         for ch in s:
             hash_map[ch] = hash_map.get(ch,0) + 1
         for ch in t:
-            hash_map[ch] = hash_map.get(ch,0) - 1
+            hash_map[ch] = hash_map.get(ch, 0) - 1
         for v in hash_map.values():
             if v != 0:
                 return False
